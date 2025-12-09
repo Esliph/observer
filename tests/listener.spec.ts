0a20740a61675868ffaa408b9896e48f9a40b4a3
Dan Ruan <danrsaquetti@gmail.com>
2025-12-09 20:45:53 -0300

    teste: Criando teste que espera que os listeners registrados para o evento sejam chamados com os dados passados

--- tests/listener.spec.ts
+++ tests/listener.spec.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, test } from 'vitest'
+import { beforeEach, describe, expect, test, vi } from 'vitest'
 
 import { Listener } from '@listener'
 import { Observer } from '@observer'
@@ -19,5 +19,26 @@ describe('Listener', () => {
     expect(listener.id).toBe(id)
     expect(listener.event).toBe('event')
     expect(typeof listener.handler).toBe('function')
+  })
+
+  test('The registered listener is expected to be called', () => {
+    const handler = vi.fn(() => { })
+
+    observer.on('event', handler)
+
+    observer.emit('event', null)
+    observer.emit('event', null)
+
+    expect(handler).toBeCalled()
+  })
+
+  test('It is expected that the data passed in the broadcast will be passed to the handler', () => {
+    const handler = vi.fn(arg => { })
+
+    observer.on('event', handler as any)
+
+    observer.emit('event', 'Hello World')
+
+    expect(handler).toHaveBeenCalledWith('Hello World')
   })
 })
